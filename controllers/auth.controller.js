734e4236bd175aee377133f026d9a8d37fabec1e
fikiaprian23 <fikiaprian23@gmail.com>
2023-12-27 20:22:11 +0700

    fixxx

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -24,7 +24,14 @@ export const signin = async (req, res, next) => {
     if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'))
     const token = jwt.sign({ id: validUser._id }, 'jwtSecret')
     const { password: pass, ...rest } = validUser._doc
-    res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest)
+    res
+      .cookie('access_token', token, {
+        httpOnly: true,
+        sameSite: 'None',
+        secure: true,
+      })
+      .status(200)
+      .json(rest)
   } catch (error) {
     next(error)
   }
