74bcd51bb47860b3fadc9fbe83e9933d1982dcd6
fikiaprian23 <fikiaprian23@gmail.com>
2023-12-27 20:10:07 +0700

    fix

--- index.js
+++ index.js
@@ -24,8 +24,20 @@ mongoose
 
 const app = express()
 
-app.use(express.json())
-app.use(cors())
+// Enable CORS for all routes with credentials support
+app.use(
+  cors({
+    origin: [
+      'https://baraya-art-v3.vercel.app',
+      'http://localhost:3000',
+      'http://localhost:5173',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  })
+)
+
+app.use(express.json({ limit: '50mb' }))
 
 app.use(cookieParser())
 
